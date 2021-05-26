e4cec5689d9666a62adc3fa4cefb9f72638aa9a0
Ilya Kisel <st.ilka@mail.ru>
2021-05-26 11:31:29 +0300

    Update Main.java
    year++ 
    
    System.out.println(day + "." + month + "." + year); once

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,30 +6,36 @@ public class Main {
         int day = 31;
         int month = 3;
         int year = 2021;
+        
+        if(day<0 || month<0 || month > 12 || year < 0) {
+            throw new IllegalArgumentException("неверно введен день/месяц");
+        }
 
         if (month == 3 && day < 31) {                                   //для марта
             ++day;
-            System.out.println(day + "." + month + "." + year);
+            
         } else if (month == 3 && day == 31) {
             day = 1;
             ++month;
-            System.out.println(day + "." + month + "." + year);
+            
         } else if (month == 4 && day < 30) {                            //для апреля
             ++day;
-            System.out.println(day + "." + month + "." + year);
+            
         } else if (month == 4 && day == 30) {
             day = 1;
             ++month;
-            System.out.println(day + "." + month + "." + year);
+            
         } else if (month == 5 && day < 31) {                            //для мая
             ++day;
-            System.out.println(day + "." + month + "." + year);
+            
         } else if (month == 5 && day == 31) {
             day = 1;
-            ++month;
-            System.out.println(day + "." + month + "." + year);
-        } else {
-            System.out.println("неверно введен день/месяц");
-        }
+            
+            year++;
+            month = 1
+           
+        } 
+        System.out.println(day + "." + month + "." + year);
+      
     }
 }
